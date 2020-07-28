4f6118374a498f7f893467415360c1b8d1e284f9
Ilya <dert_94@mail.ru>
2020-07-28 16:57:43 +0300

    Реактивное редактирование категорий

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,35 +1,54 @@
 <template>
-    <div>
-  <div class="page-title">
-    <h3>Категории</h3>
-  </div>
-  <section>
-    <div class="row">
-      <CategoryCreate @created='addNewCategories' />
-
-      <CategoryEdit />
+  <div>
+    <div class="page-title">
+      <h3>Категории</h3>
     </div>
-  </section>
-</div>
+    <section>
+      <Loader v-if="loading" />
+      <div v-else class="row">
+        <CategoryCreate @created="addNewCategories" />
+
+        <CategoryEdit 
+        v-if='categories.length'
+        :categories="categories" 
+        :key='categories.length + updateCount'
+        @updated="updateCategories" 
+        />
+        <p v-else class='center'>Категорий нет </p>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-import CategoryCreate from '@/components/CategoryCreate'
-import CategoryEdit from '@/components/CategoryEdit'
+import CategoryCreate from "@/components/CategoryCreate";
+import CategoryEdit from "@/components/CategoryEdit";
 export default {
-  name: 'categories',
+  name: "categories",
   data: () => ({
-    categories: []
+    categories: [],
+    loading: true,
+    updateCount: 0
   }),
-  components: {
-    CategoryCreate, CategoryEdit
+  async mounted() {
+    this.categories = await this.$store.dispatch("fetchCategories");
+    this.loading = false;
   },
-  methods:{
+  components: {
+    CategoryCreate,
+    CategoryEdit,
+  },
+  methods: {
     addNewCategories(category) {
-      this.categories.push(category)
-      console.log(this.categories)
-    }
-  }
-  
-}
+      this.categories.push(category);
+      console.log(this.categories);
+    },
+    updateCategories(category) {
+      const idx = this.categories.findIndex((c) => c.id === category.id);
+      this.categories[idx].title = category.title;
+      this.categories[idx].limit = category.limit;
+      this.updateCount++
+    },
+  },
+};
 </script>
